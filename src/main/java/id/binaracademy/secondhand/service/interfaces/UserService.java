cb11a63dd348a1f3cbc56c0ebb13bf9a577827b1
Irfan Azhar <muhammad.irfan.azhar@mail.ugm.ac.id>
2022-07-05 09:03:18 +0700

    (feat/user) fixing bugs and remove unimportant method on user api

--- src/main/java/id/binaracademy/secondhand/service/interfaces/UserService.java
+++ src/main/java/id/binaracademy/secondhand/service/interfaces/UserService.java
@@ -1,30 +1,25 @@
 package id.binaracademy.secondhand.service.interfaces;
 
 
-import id.binaracademy.secondhand.dto.UserInfoDto;
+import id.binaracademy.secondhand.dto.UpdateUserInfoDto;
 import id.binaracademy.secondhand.dto.UserRegisterDto;
 import id.binaracademy.secondhand.entity.User;
+import id.binaracademy.secondhand.entity.UserInfo;
 import org.springframework.data.domain.Page;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
 
 public interface UserService {
     User saveUser(UserRegisterDto user);
-    User findUserById(Long id);
+    UserInfo findUserById(Long id);
     User findUserByUsername(String username);
     User findUserByEmail(String email);
-    Page<User> findAllUsers(int page, int size, String sortBy, String sortType);
-    User updateUser(Long id, UserRegisterDto user);
+    Page<UserInfo> findAllUsers(int page, int size, String sortBy, String sortType);
+    UserInfo updateUser(Long id, UpdateUserInfoDto user);
     void deleteUser(Long id);
-    User addRoleToUser(Long userId, String roleName);
-    String login(String username, String password);
-    UserInfoDto findUserInfoDtoById(Long id);
-    List<UserInfoDto> findAllUserInfoDtos();
-
-    User updateUserInfo(Long id, UserInfoDto userInfoDto);
+    UserInfo registerAsSeller(Long userId);
     void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException;
 }
 
