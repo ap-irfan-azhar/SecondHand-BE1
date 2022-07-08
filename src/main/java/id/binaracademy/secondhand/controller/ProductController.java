fa252e09421a134ce68e81a2cab51c66c1012c9f
rizk-yulianto <99592851+rizk-yulianto@users.noreply.github.com>
2022-07-08 21:09:13 +0700

    pathvariable

--- src/main/java/id/binaracademy/secondhand/controller/ProductController.java
+++ src/main/java/id/binaracademy/secondhand/controller/ProductController.java
@@ -65,7 +65,7 @@ public class ProductController {
 
     @CrossOrigin
     @RequestMapping(value = "/update1", method = RequestMethod.PUT, consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
-    public Product updateProduct1(@PathVariable Long id, @ModelAttribute ProductDto product) throws Exception {
+    public Product updateProduct1(@RequestParam Long id, @ModelAttribute ProductDto product) throws Exception {
         return productService.updateProduct(id, product);
     }
 
